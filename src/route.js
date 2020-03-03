e05deff0273a6b91bb3305fcc49d489713a31920
Amjed Agabani <amjed.agabani@centtrip.com>
2020-03-03 15:27:38 +0000

    created route.js and created a graph for route assignment

---
+++ src/route.js
@@ -0,0 +1,14 @@
+class Route {
+
+    constructor(routeNumber, doorA, doorB) {
+        this.routeNumber = routeNumber;
+        this.doorA = doorA;
+        this.doorB = doorB;
+
+    }
+
+    useRoute(routeNumber){
+        // spawn 
+    }
+
+}
